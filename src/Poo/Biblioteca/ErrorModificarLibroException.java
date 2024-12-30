ee43953ee8f2eff2156a16b155059898dc838a35
Ymko.bami_ <laura2005vaquero@gmail.com>
2024-12-30 16:44:35 +0100

    Biblioteca v2

--- src/Poo/Biblioteca/ErrorModificarLibroException.java
+++
@@ -1,7 +0,0 @@
-package Poo.Biblioteca;
-
-public class ErrorModificarLibroException extends Exception{
-    public String ErrorModificarLibroException(String message){
-        return "Error al modificar el libro.";
-    }
-}
--- src/Poo/Biblioteca/LibroEliminadoException.java
+++
@@ -1,7 +0,0 @@
-package Poo.Biblioteca;
-
-public class LibroEliminadoException extends Exception{
-    public String LibroEliminadoException (String message){
-        return "El libro no ha podido ser eliminado.";
-    }
-}
